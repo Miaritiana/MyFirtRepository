b793e0cf0b49dab611d493858e558f69c12a34ae
Miaritiana <97837302+Miaritiana@users.noreply.github.com>
2022-03-19 07:37:52 +0300

    feat: creat new file index.php

---
+++ index.php
@@ -0,0 +1,425 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="fr">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Haute Ecole d'Informatique - [HEI]</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="css/bootstrap-grid.css">
+<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<!-- Header Section -->
+<section class="banner" role="banner"> 
+  <!-- Navigation Section -->
+  <header id="header">
+    <div class="header-content clearfix">
+      <a class="logo" href="#"><img src="images/logohei.png" alt="" class="logo"></a>
+      <nav class="navigation">
+        <ul class="primary-nav">
+          <li><h5><a href="#introduction">Accueil</a></h5></li>
+          <li><h5><a href="#works">Formation</a></h5></li>
+          <li><h5><a href="#concours">Concours</a></h5></li>
+          <li><h5><a href="#contact">Contacts</a></h5></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a>
+    </div>
+  </header>
+  <!-- Navigation Section --> 
+  <!-- Banner Section -->
+  <div class="container">
+    <div class="col-md-7 banner-inner-wrapper">
+      <div class="banner-text">
+        <h1>Haute Ecole d’Informatique</h1>
+        <p>Professionnels du monde entier unis pour former les étudiants malgaches suivant les plus hauts standards</p>
+        <a href="#works" class="btn">Notre programme</a> </div>
+      <!-- banner text --> 
+    </div>
+  </div>
+  <!-- Banner Section --> 
+</section>
+<!-- Header Section --> 
+<!-- Intro Section -->
+<section id="introduction" class="section introduction">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6">
+        <div class="intro-content">
+          <h1>A propos de nous</h1>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-6">
+        <div class="intro-content">
+          <p>La Haute École d'Informatique - HEI de Madagascar est un établissement privé délivrant un diplôme de Bac+3 en Informatique.</p>
+          <p>HEI forme des futurs développeurs-concepteurs d'applications et des as du marketing digital sous les standards internationaux.</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="intro-content">
+          <p>Des étudiants à Madagascar formés par un ingénieur Google à Londres, un polytechnicien de Montréal et beaucoup d'autres professionnels du monde entier. HEI produit l'excellence à tout prix, mais la rend accessible pour tous les prix. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Intro Section --> 
+
+<!-- work section -->
+<section id="works" class="works section no-padding">
+  <h1 class="titreformation">Notre formation</h1>
+  <h2 class="prof">Les equipes pedagogiques</h2><br>
+  <div class="container-fluid prof">
+    <div class="row no-gutter">
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/10505034_1098877990141738_66341044120357.webp" class="work-box"> <img src="images/10505034_1098877990141738_66341044120357.webp" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mr Parison RAVALOMANDA</p>
+            <p><u>Responsable:</u></p>
+            <p>PROG4</p>
+            <p>PROG5</p>
+            <p>PROG7</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/26911018_2234084620151779_31014442009070.webp" class="work-box"> <img src="images/26911018_2234084620151779_31014442009070.webp" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Dre Tahina RALITERA</p>
+            <p><u>Responsable:</u></p>
+            <p>METIER2</p>
+            <p>MGT3</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/Sandrin.webp" class="work-box"> <img src="images/Sandrin.webp" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mme Sandrine RAMAROSON</p>
+            <p><u>Responsable:</u></p>
+            <p>MGT1</p>
+            <p>DONNEES1</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/harry.PNG" class="work-box"> <img src="images/harry.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mr Bako Harry RAKOTONDRATOMPO</p>
+            <p><u>Responsable:</u></p>
+            <p>SYS1</p>
+            <p>SYS2</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/tokimahery.PNG" class="work-box"> <img src="images/tokimahery.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>MrTokimahery RAMAROZAKA</p>
+            <p><u>Responsable:</u></p>
+            <p>WEB1</p>
+            <p>PROG1</p>
+            <p>PROG3</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/fondateur.PNG" class="work-box"> <img src="images/fondateur.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mr Fonenantsoa MAURICA</p>
+            <p>Fondateur HEI</p>
+            <p><u>Responsable:</u></p>
+            <p>WEB3</p>
+            <p>WEB4</p>
+            <p>PROG7</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="" class="work-box"> <img src="images/blanc.PNG" alt="">
+        <div class="">
+          <div class="overlay-caption">
+            <!--<p><i class="fa fa-search-plus fa-2x"></i></p>-->
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/andriambololona.PNG" class="work-box"> <img src="images/andriambololona.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+           <p>Mme Fenitra ANDRIAMBOLOLONA</p>
+           <p><u>Responsable:</u></p>
+           <p>DONNEES3</p>
+           <p>METIER1</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/velontrasina.PNG" class="work-box"> <img src="images/velontrasina.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mme Irene VELONTRASINA</p>
+            <p><u>Responsable:</u></p>
+            <p>MGT2</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/rakotomandimby.PNG" class="work-box"> <img src="images/rakotomandimby.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mihamina RAKOTOMANDIMBY</p>
+            <p><u>Responsable:</u></p>
+            <p>DONNEES2</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="images/ryan.PNG" class="work-box"> <img src="images/ryan.PNG" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p>Mr Ryan ANDRIAMAHERY</p>
+            <p>Co-fondateur HEI</p>
+            <p><u>Responsable:</u></p>
+            <p>THEORIE1</p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-2 col-md-4 col-sm-4 work"> <a href="" class="work-box"> <img src="images/blanc.PNG" alt="">
+        <div class="">
+          <div class="overlay-caption">
+            <!-- <p><i class="fa fa-search-plus fa-2x"></i></p> -->
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+    </div>
+  </div>
+  <div class="cours">
+    <h2>Les cours</h2><br>
+    <p>Notre formation est divisée en trois parties</p>
+    <ul>
+      <li><p>Travaux théoriques</p></li>
+      <li><p>Travaux dirigées</p></li>
+      <li><p>Travaux personnels</p></li>
+    </ul>
+    <p>Les cours théoriques sont composés de tout document (slides, pdf, livres etc ...) ou vidéo illustrant la partie théorique des TD que vous ferez en présentiel (2 heures par jours pour chaque matière), afin que vous puissiez d'abord comprendre la théorie avant de passer à la pratique, voire même anticiper certains cours.Pour chaque cours théorique diffusé, les TD relatifs à ceux-ci seront effectués la semaine d'après la diffusion.</p>
+  </div>
+  <div class="parcours">
+    <h2>les parcours</h2>
+    <p>HEI propose deux parcours : Ecosystèmes Logiciels (EL) et Transformations Numériques (TN).</p>
+    <p>Le parcours EL produit des profils capables de programmer les briques de systèmes informatiques
+complexe. Il mène aux débouchés suivants : développeur backend, développeur frontend,
+développeur mobile, analyste de données, chef de projet, consultant en innovation.
+Le parcours TN produit des profils capable d’exploiter les briques déjà programmées de systèmes
+informatiques complexes. Il mène aux débouchés suivants : référenceur web, gestionnaire web,
+commerçant-marketeur digital, analyste de données, devOps, chef de projet, consultant en
+innovation.</p>
+  </div>
+  <div class="UE">
+    <h2>Les unités d’enseignements</h2>
+      <div class="d-flex bd-highlight">
+        <div class="p-2 flex-fill bd-highlight">
+          <h3>En L1</h3>
+            <h4 class="types">Tronc commun:</h4>
+              <div class="liste">
+                <p>WEB1: Interfaces web</p>
+                <p>PROG1: Algorithmique</p>
+                <p>THEORIE1: Mathématiques de l’informatique</p>
+                <p>SYS1: Systèmes d’exploitation</p>
+                <p>MGT1: Travail collaboratif</p>
+                <p>SYS2: Systèmes interconnectés</p>
+                <p>PROG2: Programmation orientée objet</p>
+                <p>DONNEES1: Bases de données non structurées</p>
+                <p>DONNEES2: Bases de données structurées</p>
+              </div>
+            <h4 class="types">Spécifique au parcours EL:</h4>
+              <div class="liste">
+                <p>THEORIE2: Analyse de complexité</p>
+                <p>PROG3: Arithmétique des machines</p>
+              </div>
+            <h4 class="types">Spécifique au parcours TN:</h4>
+              <div class="liste">
+                <p>WEB2: Commerce et marketing digital </p>
+              </div>
+          </div>
+          <div class="p-2 flex-fill bd-highlight">
+            <h3>En L2</h3>
+            <h4 class="types">Tronc commun:</h4>
+              <div class="liste">
+                <p>IA1: Intelligence artificielle symbolique</p>
+              </div>
+            <h4 class="types">Spécifique au parcours EL:</h4>
+              <div class="liste">
+                <p>IHM2: Interfaces mobiles</p>
+                <p>PROG4: Programmation fonctionnelle</p>
+                <p>PROG5: Algorithmique répartie</p>
+                <p>PROG6: Qualité du code</p>
+                <p>WEB4: Services web et API </p>
+              </div>
+            <h4 class="types">Spécifique au parcours TN:</h4>
+                <div class="liste">
+                  <p>WEB3: Présence en ligne</p>
+                  <p>METIER1: ERP et dérivés</p>
+                  <p>METIER2: Systèmes d'Information Géographique</p>
+                  <p>DONNEES3: ETL-EAI-ESB</p>
+                </div>
+          </div>
+          <div class="p-2 flex-fill bd-highlight">
+            <h3>En L3</h3>
+            <h4 class="types">Tronc commun:</h4>
+                <div class="liste">
+                  <p>IA2: Intelligence artificielle statistique</p>
+                  <p>MGT2: Rédaction technique</p>
+                  <p>MGT3: Gestion de projet</p>
+                  <p>MGT4: Communication en entreprise</p>
+                </div>
+            <h4 class="types">Spécifique au parcours EL:</h4>
+                <div class="liste">
+                  <p>PROG7: Architectures logicielles</p>
+                </div>
+            <h4 class="types">Spécifique au parcours TN:</h4>
+                <div class="liste">
+                  <p>DONNEES4: Business Intelligence</p>
+                </div>
+          </div>
+        </div>
+        <p>Pour plus d'information telecharger notre <a href="https://drive.google.com/file/d/12Lc4o3jfQOFHIzazPToO2hnGZc8epU3I/view">programme</a> ou <a href="#contact">contactez-nous</a></p>
+    <h3>Alliance Française</h3>
+    <p>Chez HEI, la communication est aussi importante que la technique ! Les cours de français sont assurés par <a href="http://www.alliancefr.mg/">’Alliance Française de Madagascar</a>.<br> Les UE sur les Langues Vivantes sont obligatoires pour chaque niveau</p>
+  </div>
+</section>
+<!-- work section -->
+<!-- concours section --> 
+<section id="concours">
+  <h1 class="titreform">Inscription aux concours de HEI</h1>
+  <p>Nous recrutons de nouveaux étudiants tout au long de l’année. Plusieurs rentrées sont prévues pour chaque année calendaire. Inscrivez-vous et nous vous informerons de la date<br> de la prochaine rentrée !</p>
+  <p>Les 3 étudiants les mieux classés au concours d’entrée se verront exonérés des frais de scolarité. Il en va de même pour 2 étudiants supplémentaires évalués sur critères sociaux.</p>
+  <p>Par ailleurs, HEI assure le financement à remboursement du Master en Informatique en France de l’étudiant(e) le plus méritant(e) à hauteur de 8.900€ grâce au programme Vatsy.</p>
+  <div class="fiche">
+  <h3>Formulaire d'inscription au concours d'entrée.</h3>
+    <p>Pour vous inscrire au concours d'entrée chez HEI :</p>
+    <ol type="1">
+      <li>Téléchargez <u>la fiche de renseignement</u></li>
+      <li>Remplissez le formulaire jusqu'à la fin</li>
+      <li>Faites le transfert MVola au +261 34 94 041 16 (au nom de Fihariantsoa)</li>
+      <li>Remplissez la dernière case avec le numéro de transfert MVola</li>
+    </ol>
+  </div>
+<div class="inscription">
+<div class="formulaire col-8">
+  <br>
+  <div class="formuone">
+    <form>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <p class="input-group-text">Nom et prenoms</p>
+        </div>
+        <input type="text" class="form-control" placeholder="Nom">
+        <input type="text" class="form-control" placeholder="Prenoms">
+      </div>
+      <div class="form-group">
+        <label>Date de naissance:</label>
+        <input type="date" class="form-control" id="naissance">
+      </div>
+      <div class="form-group">
+        <label>CIN(Pour les majeurs):</label>
+        <input type="number" class="form-control" id="cin">
+      </div>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <p class="input-group-text">Année d'obtention du BACC</p>
+        </div>
+        <input type="number" class="form-control">
+      </div>
+      <div class="d-flex">
+        <div class="flex-fill">
+          <p>Série BACC</p>
+          <div>
+            <input type="radio" id="A1" name="Série">
+            <label for="A1">Série A1</label>
+          </div>
+          <div>
+            <input type="radio" id="A2" name="Série">
+            <label for="A2">Série A2</label>
+          </div>
+          <div>
+            <input type="radio" id="C" name="Série">
+            <label for="C">Série C</label>
+          </div>
+          <div>
+            <input type="radio" id="D" name="Série">
+            <label for="D">Série D</label>
+          </div>
+          <div>
+            <input type="radio" id="Technique" name="Série">
+            <label for="Technique">Technique</label>
+          </div>
+          <div>
+            <input type="radio" id="Autre" name="Série">
+            <label for="Autre">Autre</label>
+          </div>
+        </div>
+        <div class="flex-fill">
+          <p>Mention au BACC </p>
+          <div>
+            <input type="radio" id="Passable" name="Mention">
+            <label for="Passable">Passable</label>
+          </div>
+          <div>
+            <input type="radio" id="AssezBien" name="Mention">
+            <label for="AssezBien">Assez Bien</label>
+          </div>
+          <div>
+            <input type="radio" id="Bien" name="Mention">
+            <label for="Bien">Bien</label>
+          </div>
+          <div>
+            <input type="radio" id="TrèsBien" name="Mention">
+            <label for="TrèsBien">Très Bien</label>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>Autre(s) diplôme(s) obtenu(s)</p>
+        <input type="checkbox" name="dip1" id="dip1">
+        <label for="dip1">BACC +2</label><br>
+        <input type="checkbox" name="dip2" id="dip2">
+        <label for="dip2">Licence</label><br>
+        <input type="checkbox" name="dip3" id="dip3">
+        <label for="dip3">Master</label><br>
+        <input type="checkbox" name="dip4" id="dip4">
+        <label for="dip4">DEA</label><br>
+        <input type="checkbox" name="dip5" id="dip5">
+        <label for="dip5">Doctorat</label><br>
+      </div>
+      <div class="form-group">
+        <p>Adresse exacte</p>
+        <input type="text" class="form-control" id="date">
+      </div>
+      <div>
+        <p>Envisagez-vous demander une bourse d’études chez HEI ?</p>
+        <input type="radio" name="answer" id="oui">
+        <label for="oui">Oui</label><br>
+        <input type="radio" name="answer" id="non">
+        <label for="non">Non</label>
+      </div>
